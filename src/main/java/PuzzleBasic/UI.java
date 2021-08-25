e68f7a840ca294c87105e5de332dc6d39aa8aa5d
Meganl <Meganl@ZAKZRHB-CLLB14D.ballito.local>
2021-08-25 07:28:46 +0200

    Made pictures switch when button is pushed

--- src/main/java/PuzzleBasic/UI.java
+++ src/main/java/PuzzleBasic/UI.java
@@ -15,25 +15,23 @@ import javax.swing.ImageIcon;
  */
 public class UI extends javax.swing.JPanel {
     
-    
-    String pic1 = "images//flowers1";
-    String pic2 = "images//flowers2";
-    
-    ImageIcon icon1 = new ImageIcon(pic1);
-    ImageIcon icon2 = new ImageIcon(pic2);
-    
+    //ImageIO.read(new File("images\\slate.png"));
+    String pic1 = "images//flowers1.png";
+    String pic2 = "images//flowers2.png";
     
     /**
      * Creates new form UI
      */
-    public UI() {
-        initComponents();
-    }
-    
-    public static void main(String[] args) 
+    public UI() 
     {
+        initComponents();
+        //setLocationRelativeTo(null);
+        setLocation(0,0);
+        
         
     }
+    
+   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -43,13 +41,13 @@ public class UI extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        Picture1Screen = new javax.swing.JLabel();
+        Pic2Scr = new javax.swing.JLabel();
         SwitchButton = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        Picture2Screen = new javax.swing.JLabel();
+        Pic1Scr = new javax.swing.JLabel();
 
-        Picture1Screen.setIcon(new javax.swing.ImageIcon("C:\\Users\\Meganl\\Documents\\NetBeansProjects\\PATDraft\\src\\main\\resources\\images\\flowers2.jpeg")); // NOI18N
-        Picture1Screen.setText("jLabel2");
+        Pic2Scr.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flowers2.jpeg"))); // NOI18N
+        Pic2Scr.setText("jLabel2");
 
         SwitchButton.setText("Switch");
         SwitchButton.addActionListener(new java.awt.event.ActionListener() {
@@ -58,8 +56,8 @@ public class UI extends javax.swing.JPanel {
             }
         });
 
-        Picture2Screen.setIcon(new javax.swing.ImageIcon("C:\\Users\\Meganl\\Documents\\NetBeansProjects\\PATDraft\\src\\main\\resources\\images\\flowers1.jpg")); // NOI18N
-        Picture2Screen.setText("jLabel1");
+        Pic1Scr.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flowers1.jpg"))); // NOI18N
+        Pic1Scr.setText("jLabel1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -67,14 +65,14 @@ public class UI extends javax.swing.JPanel {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Picture2Screen, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Pic1Scr, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Picture2Screen, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(Pic1Scr, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(21, Short.MAX_VALUE))
         );
 
@@ -87,7 +85,7 @@ public class UI extends javax.swing.JPanel {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(2, 2, 2)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(Picture1Screen, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Pic2Scr, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(SwitchButton))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -100,7 +98,7 @@ public class UI extends javax.swing.JPanel {
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(27, 27, 27)
-                        .addComponent(Picture1Screen, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(Pic2Scr, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(SwitchButton)))
                 .addContainerGap(25, Short.MAX_VALUE))
@@ -110,12 +108,16 @@ public class UI extends javax.swing.JPanel {
     private void SwitchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SwitchButtonActionPerformed
         // TODO add your handling code here:
         //label.setIcon(finalIcon);
+        
+        Pic1Scr.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flowers2.jpeg")));
+        Pic2Scr.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/flowers1.jpg")));
+
     }//GEN-LAST:event_SwitchButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Picture1Screen;
-    private javax.swing.JLabel Picture2Screen;
+    private javax.swing.JLabel Pic1Scr;
+    private javax.swing.JLabel Pic2Scr;
     private javax.swing.JButton SwitchButton;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
