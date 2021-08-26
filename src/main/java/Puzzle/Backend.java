01b8989695f2da8c47dd7badda75bb815c305a58
Meganl <Meganl@ZAKZRHB-CLLB14D.ballito.local>
2021-08-26 08:30:43 +0200

    Started making full puzzle

---
+++ src/main/java/Puzzle/Backend.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Puzzle;
+
+/**
+ *
+ * @author Meganl
+ */
+public class Backend 
+{
+    //need a picture randomiser
+    public static int PictureR
+    //picture swapper
+    //check if pic3 is next to picture check (pic3 is blank)
+    
+}
